c44be18cf629f64cdc94f3a2c018caa361b1aacb
Menno <114731270+Buldz@users.noreply.github.com>
2022-12-02 10:21:47 +0100

    scene

--- A-O-S/Assets/Code/LevelManager.cs
+++ A-O-S/Assets/Code/LevelManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,8 +6,16 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
+
+    //laad volgende scene
   public void LoadNextScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    //Sluit het spel af
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
